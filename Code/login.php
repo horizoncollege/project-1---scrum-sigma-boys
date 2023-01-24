dfb1136101958785fc3890af5313ef6d19935b53
RichenelWartim <114988676+RichenelWartim@users.noreply.github.com>
2023-01-24 11:05:18 +0100

    Add files via upload

--- Code/login.php
+++ Code/login.php
@@ -1,5 +1,7 @@
 <?php
 
+session_start();
+
 $host = 'localhost';
 $db   = 's168308_project';
 $user = 's168308_Project';
@@ -19,7 +21,6 @@ try {
 } catch (\PDOException $e) {
     throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
-
 
 
 ?>
@@ -96,52 +97,27 @@ try {
         <input type="text" id=lettters name="name" placeholder="gebruikersnaam" required>
         <input type="password" id=lettters name="pass" placeholder="Wachtwoord" required>
         <input type="submit" id=inloggen name="login" value="Inloggen" required>
-
         <a href="registratie.php" id=create>nog geen account? <span>account aanmaken</span> </a>
     </Form>
     <?php
-    $stmt = $pdo->prepare("SELECT * FROM users");
-    $stmt->execute();
-    $gebruikers = $stmt->fetch(PDO::FETCH_OBJ);
-
-    function getUsername()
-    {
-        global $gebruikers;
-        return  $gebruikers->username;
-    }
-    function getEmail()
-    {
-        global $gebruikers;
-        return $gebruikers->emailAddress;
-    }
-    function getPassword()
-    {
-        global $gebruikers;
-        return $gebruikers->wachtwoord;
-    }
-
-    $user = getUsername();
-    $ww = getPassword();
-    $email = getEmail();
-    if (isset($_POST["login"])) {
+    // aquires input/database information
+    if (isset($_POST['name']) && isset($_POST['pass'])) {
         $username = $_POST['name'];
         $password = $_POST['pass'];
-        if ($username == $user && $password == $ww) {
-            $_SESSION['userlogin'] = $username;
+        $query = $pdo->query("SELECT * FROM users WHERE username = '$username' AND wachtwoord = '$password' ");
+        $user = $query->fetch();
+        // checks if input = database
+        if ($user !== false) {
+            $_SESSION['loggedInUser'] = $user['username'];
         } else {
-
-            echo "<h1 id='denied'>Invalide gebruikersnaam of wachtwoord combinatie</h1>";
-
             $_SESSION['error'] = "<h1 id=denied>Gebruikersnaam of wachtwoord is ongeldig. </h1>";
             echo '<script>alert("wrong Username or password")</script> ';
         }
     }
-
-
-
-    if (isset($_SESSION['userlogin'])) {
-        echo "<h1 id='denied'>welcome $user</h1>";
-        header("Refresh:1; url=index.php");
+    // check if session is active and sends user to index
+    if (isset($_SESSION['loggedInUser'])) {
+        echo $_SESSION['user'] = $username;
+        header("Refresh:0; url=index.php");
     }
     ?>
     <footer>
