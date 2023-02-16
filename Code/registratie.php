6a676cb3fd5936e2cb1ece8c93dc10bf16ebc6b9
justinhorizon <114988813+justinhorizon@users.noreply.github.com>
2023-02-16 08:30:11 +0100

    encrypte

--- Code/registratie.php
+++ Code/registratie.php
@@ -40,153 +40,57 @@ try {
 
 <body>
 
-<div class="topnav">
-        <a href="index.php" class="active">SIGMA MEDIA</a>
+    <div class="topnav">
+        <a href="index.php" class="active">Sigma media</a>
         <!-- Navigation links (hidden by default) -->
         <div id="myLinks">
-            <?php if (!isset($_SESSION['loggedInUser'])) {
-                echo '<h1 id= "nav-color">hallo gebruiker</h1>';
-            }
-            if (isset($_SESSION['loggedInUser'])) {
-                echo '<h1 id= "nav-color"> Welkom ' . $_SESSION['user'] . '</h1>';
-            }
-
-            ?>
             <a href="film.php">FILMS</a>
-            <a href="musical.php">MUSICALS</a>
+            <a href="musical.php" id="MusicalCurrentPage">MUSICALS</a>
             <a href="Concerten.php">CONCERTEN</a>
             <a href="events.php">EVENTS</a>
-            <?php if (!isset($_SESSION['loggedInUser'])) {
-                echo ' <a href="login.php">INLOGGEN</a>';
-            }
-            if (isset($_SESSION['loggedInUser'])) {
-                echo ' <a href="logout.php" onClick="return confirmLogout()">UITLOGGEN</a>';
-            }
-
-            ?>
-
         </div>
         <!-- "Hamburger menu" / "Bar icon" to toggle the navigation links -->
         <a href="javascript:void(0);" class="icon" onclick="myFunction()">
             <i class="fa fa-bars"></i>
         </a>
     </div>
-
-    <header>
-        <header class="Header">
-            <div id="container">
-                <div id="Sigma">
-                    <a href="index.php" id="nav-color">
-                        <h1>SIGMA MEDIA</h1>
-                    </a>
-                </div>
-                <div id="nav-bar" class="menu">
-                    <a href="film.php" class="menuItem" id="nav-color">
-                        <h2>FILMS</h2>
-                    </a>
-                    <a>
-                        <h2 class="line" id="nav-color">|</h2>
-                    </a>
-                    <a href="musical.php" class="menuItem" id="nav-color">
-                        <h2>MUSICALS</h2>
-                    </a>
-                    <a>
-                        <h2 class="line" id="nav-color">|</h2>
-                    </a>
-                    <a href="Concerten.php" class="menuItem" id="nav-color">
-                        <h2>CONCERTEN</h2>
-                    </a>
-                    <a>
-                        <h2 class="line" id="nav-color">|</h2>
-                    </a>
-                    <a href="events.php" class="menuItem" id="nav-color">
-                        <h2>EVENTS</h2>
-                    </a>
-
-                    </a>
-                </div>
-                <!-- Hamburger -->
-                <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-                    <i class="fa fa-bars"></i>
+    <!-- navigatie bar-->
+    <header class="Header">
+        <div id="container">
+            <div id="Sigma">
+                <a href="index.php" id="nav-color">
+                    <h1>SIGMA MEDIA</h1>
                 </a>
-                <!-- show/hide login button -->
-                <div id="login">
-                    <?php if (!isset($_SESSION['loggedInUser'])) {
-                        echo '<a href="login.php" id="login-Guest"> <img src="Img/admin.png" alt="Login_button">
-                        </a>';
-                    }
-                    // Checks if logged in
-                    if (isset($_SESSION['loggedInUser'])) {
-                        $username = $_SESSION['user'];
-                        $userquery = $pdo->prepare("SELECT * FROM users WHERE username = '$username'");
-                        $userquery->execute();
-                        $userAdmin = $userquery->fetch(PDO::FETCH_OBJ);
-
-                        function getPoster()
-                        {
-                            global $userAdmin;
-                            return $userAdmin->isAdmin;
-                        }
-                        $userAdminNumber = getPoster();
-                        // checks user number and grats priveledges
-                        if ($userAdminNumber == 2) {
-                            echo '<h1 id="nav-color">' . $_SESSION['user'] . '
-                        <div class="dropdown">
-                        <img src="Img/admin.png" id="login" alt="Login_button">
-                            <div class="dropdown-content">
-                                <a href="logout.php" onClick="return confirmLogout()">uitloggen</a>
-                                <a href="CreateEvent.php">create event</a>
-                                <a href="addAdmin.php" id=FilmsCurrentPage >Add admin</a>
-                            </div>
-                        </div>';
-                        } elseif ($userAdminNumber == 1) {
-                            echo '<h1 id="nav-color">' . $_SESSION['user'] . '
-                        <div class="dropdown">
-                        <img src="Img/admin.png" id="login" alt="Login_button">
-                            <div class="dropdown-content">
-                                <a href="logout.php" onClick="return confirmLogout()">uitloggen</a>
-                                <a href="CreateEvent.php">create event</a>  
-                            </div>
-                        </div>';
-                        } elseif ($userAdminNumber == 0) {
-                            echo '<h1 id="nav-color">' . $_SESSION['user'] . '
-                            <div class="dropdown">
-                            <img src="Img/admin.png" id="login" alt="Login_button">
-                                <div class="dropdown-content">
-                                    <a href="logout.php" onClick="return confirmLogout()">uitloggen</a>
-                                </div>
-                            </div>';
-                        }
-                    }
-
-                    ?>
-                    <!-- log out confirmation -->
-                    <script language="JavaScript">
-                        function confirmLogout() {
-
-                            if (!confirm("Are you sure you want to log out?")) {
-
-                                return false;
-
-                            }
-                        }
-                    </script>
-
-                </div>
-                <!-- moving shopping card as required -->
-                <?php if (!isset($_SESSION['loggedInUser'])) {
-                    echo '<a href="shopping.php" id="mandje"> <img src="Img/cart.png" alt="shopping_button"></a>';
-                }
-                if (isset($_SESSION['loggedInUser'])) {
-                    echo '<a href="shopping.php" id="mandje-user"> <img src="Img/cart.png" alt="shopping_button"></a>';
-                }
-
-                ?>
             </div>
+            <div id="nav-bar">
+                <a href="film.php" id="nav-color">
+                    <h2>FILMS</h2>
+                </a>
+                <a>
+                    <h2 id="nav-color">|</h2>
+                </a>
+                <a href="musical.php" id="nav-color">
+                    <h2 id="MusicalCurrentPage">MUSICALS</h2>
+                </a>
+                <a>
+                    <h2 id="nav-color">|</h2>
+                </a>
+                <a href="Concerten.php" id="nav-color">
+                    <h2 id="ConcertenCurrentPage">CONCERTEN</h2>
+                </a>
+                <a>
+                    <h2 id="nav-color">|</h2>
+                </a>
+                <a href="events.php" id="nav-color">
+                    <h2>EVENTS</h2>
+                </a>
 
+                </a>
+            </div>
+        </div>
 
-        </header> 
-        <Form id="Formcontainer" method="POST">
+    </header>
+    <Form id="Formcontainer" method="POST">
         <input type="text" id=lettters name="user" placeholder="gebruikersnaam" required>
         <input type="email" id=lettters name="email" placeholder="Email-adres " required>
         <input type="text" id=lettters name="pass" placeholder="Wachtwoord" required>
@@ -240,7 +144,7 @@ try {
         <a id="contact-color">
             <h2>|</h2>
         </a>
-        <a href="Sales.php" id="contact-color">
+        <a href="Hobby.html" id="contact-color">
             <h2>Sales</h2>
         </a>
 
